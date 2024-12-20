1d7db83b839a2eb6709a5c5bdd0a2441d4fac0c5
Kaylem <kaylem20201@gmail.com>
2024-12-20 14:22:22 -0700

    Day 3 solved

--- src/solvers/day_3.rs
+++ src/solvers/day_3.rs
@@ -6,12 +6,13 @@ struct ParsedInput {
 
 pub fn solve(input: String) -> (String, String) {
 
+    let valid_sections = validate_sections(&input);
     let parsed_input = parse_input(&input);
 
     println!("Parsed input.");
 
     let part1_answer = part_1(&parsed_input);
-    let part2_answer = part_2(&parsed_input);
+    let part2_answer = part_2(valid_sections);
 
     return (part1_answer, part2_answer);
 
@@ -26,8 +27,12 @@ fn part_1(input: &ParsedInput) -> String {
     return res.to_string();
 }
 
-fn part_2(input: &ParsedInput) -> String {
-    return String::from("Not yet implemented");
+fn part_2(valid_sections: Vec<String>) -> String {
+    let mul_pairs: Vec<(u32, u32)> = valid_sections.iter().flat_map(|sec| {
+        parse_input(&sec).mul_pairs
+    }).collect();
+    let input = ParsedInput { mul_pairs };
+    part_1(&input)
 }
 
 fn parse_input(input: &str) -> ParsedInput {
@@ -39,4 +44,33 @@ fn parse_input(input: &str) -> ParsedInput {
     }).collect();
     return ParsedInput { mul_pairs };
 
+}
+
+fn validate_sections(input: &str) -> Vec<String> {
+    let do_pat = r"do()";
+    let dont_pat = r"don't()";
+    let mut valid_sections: Vec<String> = Vec::new();
+    let mut state_valid = true;
+    let mut current_buffer = String::new();
+    let input_chars = input.chars();
+    for char in input_chars {
+        current_buffer.push(char);
+        if state_valid {
+            if current_buffer.ends_with(dont_pat) {
+                state_valid = false;
+                valid_sections.push(current_buffer);
+                current_buffer = String::new();
+            }
+            continue;
+        }
+        if current_buffer.ends_with(do_pat) {
+            state_valid = true;
+            current_buffer = String::new();
+        }
+    }
+    if state_valid {
+        valid_sections.push(current_buffer);
+    }
+    // println!("Valid Sections: {:?}", valid_sections);
+    return valid_sections;
 }
